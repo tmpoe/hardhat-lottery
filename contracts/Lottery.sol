7bc119520e0df57692272219d5aa05c3690760fd
tmpITK <torok.mark.patrik@gmail.com>
2022-11-29 17:41:27 +0100

    Add base implementation and start randomness implementation

---
+++ contracts/Lottery.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+error Lotter__NotEnoughFeeEntered();
+
+contract Lottery is VRFConsumerBaseV2 {
+    uint256 immutable i_entranceFee;
+    address payable[] s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
+    event LotteryEnter(address indexed player);
+    event RequestedLotteryWinner(uint256 indexed requestId);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function enterLotter() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Lotter__NotEnoughFeeEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit LotteryEnter(msg.sender);
+    }
+
+    function requestRandomWinner() external {
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+
+        emit RequestedLotteryWinner(requestId);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
