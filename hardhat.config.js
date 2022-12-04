becad020cc915d6feb3f7eac98affee5e3344997
tmpITK <torok.mark.patrik@gmail.com>
2022-12-04 10:57:53 +0100

    Add mock contract plus minor tweaks to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,25 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.7",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            chainId: 5,
+            accounts: [GOERLI_PRIVATE_KEY],
+        },
+    },
     namedAccounts: {
         deployer: {
             default: 0,
